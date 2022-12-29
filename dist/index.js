e20c27406fc6e89d527d6cc374c3f5ec69fd3496
Samuel Velasco <samuelvelasco2698@gmail.com>
2022-12-29 10:20:34 +0100

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+class User {
+    constructor(email, name) {
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+}
+const samuel = new User("s@s.com", "Samuel");
+// samuel.city = "Caracas";
+samuel.email = "sam@bg.com";
