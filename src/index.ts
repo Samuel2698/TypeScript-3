7440f553885b72784206092249aad28bdc2ba2e3
Samuel Velasco <samuelvelasco2698@gmail.com>
2023-01-03 12:31:44 +0100

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
+// class User {
+//   public email: string;
+//   private name: string;
+//   private readonly city: string = "";
+//   constructor(email: string, name: string) {
+//     this.email = email;
+//     this.name = name;
+//   }
+// }
+
 class User {
-  email: string;
-  name: string;
-  readonly city: string = "";
-  constructor(email: string, name: string) {
-    this.email = email;
-    this.name = name;
-  }
+  readonly city: string = "Caracas";
+  constructor(
+    public email: string,
+    public name: string // private userId: number
+  ) {}
 }
 
 const samuel = new User("s@s.com", "Samuel");
 
 // samuel.city = "Caracas";
 samuel.email = "sam@bg.com";
+// samuel.name;
